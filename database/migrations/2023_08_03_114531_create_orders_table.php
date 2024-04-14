d0390d905816f347060732a0d60ffb6549a69a2d
Nik Linders <info@eleven59.nl>
2024-04-14 16:14:39 +0200

    Fixed foreign key error for newer laravel installs

--- database/migrations/2023_08_03_114531_create_orders_table.php
+++ database/migrations/2023_08_03_114531_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->text('order_summary')->nullable();
-            $table->integer('user_id')->unsigned()->nullable();
+            $table->bigInteger('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
             $table->string('name')->nullable();
             $table->string('email')->nullable();
